eee08f5bfccb9e681fe3600f3d0d5a51707a2d9f
Graehm <gpatrellfazio@gmail.com>
2023-06-02 18:19:02 -0500

    complete

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
+let notBooks = [];
 // Index get
-app.get('/items', (req, res) => {
+app.get('/notBooks', (req, res) => {
     try {
-
+        res.json(notBooks)
     } catch (error) {
-
+        res.status(400).json({message: error.message})
     }
 })
 // New get
 // Delete delete
-app.delete('/items/:id', async (req, res) => {
+app.delete('/notBooks/:id', async (req, res) => {
     try {
-        
+        const index = notBooks.findIndex(notABook => notABook.id === parseInt(req.params.id))
+        if(index === -1) return
     } catch (error) {
-        
+        res.status(400).json({message: error.message})
     }
 })
 // Update put 
-// Create post
-app.post('/items', (req, res) => {
+app.put('/notBooks/:id', async (req, res) => {
     try {
-        
+        const notABook = notBooks.find(notABook => notABook.id === parseInt(req.params.id))
     } catch (error) {
-        
+        res.status(400).json({message: error.message})
+    }
+})
+// Create post
+app.post('/notBooks', (req, res) => {
+    try {
+        notBooks.push(req.body)
+    } catch (error) {
+        res.status(400).json({message: error.message})
     }
 })
 // Edit get
 // Show  get
-app.get('/items/:id', async (req, res) => {
+app.get('/notBooks/:id', async (req, res) => {
     try {
-        
+        const notABook = notBooks.find(notABook => notABook.id === parseInt(req.params.id))
     } catch (error) {
-        
+        res.status(400).json({message: error.message})
     }
 })
 
