f9ea357938d94a89a76b690ca0c34b7fa426fe40
Graehm <gpatrellfazio@gmail.com>
2023-06-02 17:50:06 -0500

    structured

--- app.js
+++ app.js
@@ -2,8 +2,47 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.json());
+
+// Index get
+app.get('/items', (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+})
+// New get
+// Delete delete
+app.delete('/items/:id', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+// Update put 
+// Create post
+app.post('/items', (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+// Edit get
+// Show  get
+app.get('/items/:id', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
 
 
-app.listen(port, () => {
+
+
+app.listen(PORT, () => {
     console.log(`Server is Serving on port ${PORT}`)
 })
